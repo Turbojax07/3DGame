03de037d6c214aafbfc5f0e1a1e5e76e2f261924
AhAcKeR9207 <turbojax07@gmail.com>
2023-12-07 15:00:24 -0500

    Adding GUI and outlining the main loop.

---
+++ src/main/java/io/github/turbojax07/Constants.java
@@ -0,0 +1,5 @@
+package io.github.turbojax07;
+
+public class Constants {
+    public static final int framesPerSecond = 60;
+}
---
+++ src/main/java/io/github/turbojax07/Main.java
@@ -0,0 +1,14 @@
+package io.github.turbojax07;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+		boolean running = true;
+
+		while (running) {
+            //getInput();
+            //updateGameState();
+            //loadWindow();
+			Thread.sleep(1 / Constants.framesPerSecond);
+		}
+	}
+}
